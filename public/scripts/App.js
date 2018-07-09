20fdec3caeb81dc3917ea58e0c1bac3e572d44c4
Nashe Osorio <nasheog2@gmail.com>
2018-07-09 18:23:33 -0500

    Creamos dos componentes con funciones y variables

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,17 +1,121 @@
-console.log("App.js esta funcionando")
+"use strict";
+
+/*babel src/App.js --out-file=public/scripts/App.js --presets=env,react --watch
+change src/App.js
+*/
+
+//live-server public
 
 //JSX se parece al html pero no lo es
 
 //Define la variable jsx que es un parrafo, que no se exporta solo
-//Esto es BABEL
+
 var template = React.createElement(
-  "p",
-  null,
-  " Esto es JSX no html,viene de App.js"
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Esto es codigo JSX y no html, esto va a cambiar en el background"
+	),
+	React.createElement(
+		"p",
+		null,
+		"OY"
+	),
+	React.createElement("input", { type: "submit", value: "Clickname" }),
+	React.createElement(
+		"ul",
+		null,
+		React.createElement(
+			"li",
+			null,
+			" Pruebalo ya"
+		),
+		React.createElement(
+			"li",
+			null,
+			" Infinidad de probalilidades"
+		),
+		React.createElement(
+			"li",
+			null,
+			" Otro articulo"
+		)
+	)
+);
+
+//String
+var activo = "Estudiante activo";
+
+//Js
+var state = {
+	username: "Nashe",
+	universidad: "ITISTMO",
+	genero: "F",
+	ciudad: "Ixtepec"
+
+};
+
+var Fecha = Date();
+
+function getUniversity() {
+	//concidional que asigna el que va despues del ?, por lo 
+	//consiguiente si es vacio y sino regresara desconocido
+	return state.universidad != "" ? state.universidad : "Desconocido";
+}
+
+function CityIs() {
+	return React.createElement(
+		"h3",
+		{ style: { color: "blue" } },
+		state.ciudad
+	);
+}
+
+function getCity() {
+	return state.ciudad != "" ? CityIs() : "Ciudad Desconocida";
+}
+
+var template2 = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"No carga con el template1"
+	),
+	React.createElement(
+		"h3",
+		null,
+		"Encabezado 3"
+	),
+	React.createElement(
+		"p",
+		null,
+		state.username
+	),
+	React.createElement(
+		"p",
+		null,
+		getUniversity()
+	),
+	React.createElement(
+		"p",
+		null,
+		state.genero
+	),
+	getCity(),
+	React.createElement(
+		"p",
+		null,
+		"Hola! La fecha es: ",
+		Fecha
+	)
 );
 
 //mandar a buscar el div con el id=app
-var appRoot= document.getElementById("app")
+var appRoot = document.getElementById("app");
 
 //objeto de ReactDOM QUE LLEVA E PARRAFO AL INDEX
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot);
---
+++ src/App.js
@@ -0,0 +1,68 @@
+/*babel src/App.js --out-file=public/scripts/App.js --presets=env,react --watch
+change src/App.js
+*/
+
+//live-server public
+
+//JSX se parece al html pero no lo es
+
+//Define la variable jsx que es un parrafo, que no se exporta solo
+
+var template = (
+				<div> 
+					<h1>Esto es codigo JSX y no html, esto va a cambiar en el background</h1> 
+					<p>OY</p>
+					<input type= "submit" value="Clickname"/>
+					<ul>
+						<li> Pruebalo ya</li>
+						<li> Infinidad de probalilidades</li>
+						<li> Otro articulo</li>
+					</ul>
+				</div>
+				);
+
+//String
+var activo = "Estudiante activo"
+
+//Js
+var state = { 
+	username: "Nashe",
+	universidad: "ITISTMO",
+	genero: "F",
+	ciudad: "Ixtepec",
+
+}
+
+var Fecha =Date()
+
+ function getUniversity(){
+ 	//concidional que asigna el que va despues del ?, por lo 
+ 	//consiguiente si es vacio y sino regresara desconocido
+ 	return state.universidad != "" ? state.universidad: "Desconocido"
+ }
+
+function CityIs(){
+	return <h3 style ={{color: "blue"}}>{state.ciudad}</h3>
+}
+
+ function getCity(){
+ 	return state.ciudad != "" ? CityIs() : "Ciudad Desconocida"
+ }
+
+var template2 = (
+				<div>
+					<h1>No carga con el template1</h1>
+					<h3>Encabezado 3</h3>
+					<p>{state.username}</p>
+					<p>{getUniversity()}</p>
+					<p>{state.genero}</p>
+					{getCity()}
+					<p>Hola! La fecha es: {Fecha}</p>
+				</div>
+				);
+
+//mandar a buscar el div con el id=app
+var appRoot= document.getElementById("app")
+
+//objeto de ReactDOM QUE LLEVA E PARRAFO AL INDEX
+ReactDOM.render(template2, appRoot)
